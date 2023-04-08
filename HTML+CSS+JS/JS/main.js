2cc12882f4525f9ee25ac005d12e97a036057aa8
Hamilton-Carvalho <hamilton.andrade99@hotmail.com>
2023-04-07 23:40:35 -0300

    logo

--- HTML+CSS+JS/JS/main.js
+++ HTML+CSS+JS/JS/main.js
@@ -1,6 +1,17 @@
+//declarando variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
 
+//Page Preloader
+window.addEventListener('load', function () {
+    var pagePreloder = document.querySelector('.jl-preloader');
+    pagePreloder.classList.add('jl-fade-out');
+ 
+    setTimeout(function () {
+        pagePreloder.style.display = 'none';
+    }, 2000);
+});
 
+//Abrindo e fechando informação de contatos
 btnContact.addEventListener('click', function(){
     var boxContact = document.querySelector('.jl-contact-info');
     boxContact.classList.toggle('jl-is-open');
