9e52f78be0b197cf3aa8fb9f3b26b8e3a41c1ee3
Hamilton-Carvalho <hamilton.andrade99@hotmail.com>
2023-04-08 23:49:19 -0300

    bottom

--- HTML+CSS+JS/JS/main.js
+++ HTML+CSS+JS/JS/main.js
@@ -1,18 +1,19 @@
-//declarando variáveis
+//Declarando Variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
 
 //Page Preloader
 window.addEventListener('load', function () {
     var pagePreloder = document.querySelector('.jl-preloader');
     pagePreloder.classList.add('jl-fade-out');
- 
+
     setTimeout(function () {
         pagePreloder.style.display = 'none';
     }, 2000);
 });
 
-//Abrindo e fechando informação de contatos
-btnContact.addEventListener('click', function(){
+
+//Abrindo e Fechando Informações de Contato
+btnContact.addEventListener('click', function () {
     var boxContact = document.querySelector('.jl-contact-info');
     boxContact.classList.toggle('jl-is-open');
     this.classList.toggle('jl-change-icon');
