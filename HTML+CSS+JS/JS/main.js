7796444cf33308ab1e9384171597d2f923620f04
Hamilton-Carvalho <hamilton.andrade99@hotmail.com>
2023-04-16 23:06:44 -0300

    JS

--- HTML+CSS+JS/JS/main.js
+++ HTML+CSS+JS/JS/main.js
@@ -1,5 +1,6 @@
 //Declarando Variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
+var toggleModal = document.querySelectorAll('.jl-toggle-modal');
 
 //Page Preloader
 window.addEventListener('load', function () {
@@ -18,3 +19,17 @@ btnContact.addEventListener('click', function () {
     boxContact.classList.toggle('jl-is-open');
     this.classList.toggle('jl-change-icon');
 });
+
+//Abrindo e Fechando o Modal de Orcamento
+
+for (var i = 0; i < toggleModal.length; i++) {
+    toggleModal[i].addEventListener('click', function () {
+        var overlay = document.querySelector('.jl-overlay');
+        var modalOrcamento = document.querySelector('#jl-modal-orcamento');
+
+        overlay.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-slide-top-in');
+
+    });
+}
